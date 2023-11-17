edafacf82da6df294ae25c916dd066b977c139bd
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2023-11-17 20:11:35 +0000

    Job done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let end = [".com", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < end.length; z++) {
+          let domain = pronoun[i] + adj[x] + noun[y] + end[z];
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
